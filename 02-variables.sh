54d887aa8c9efc7f671bf0e10cb6bb778bb61b14
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2024-03-08 10:09:47 +0530

    03-variables-commandline

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 PERSON1=Suresh
 PERSON2=Ramesh
 
---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am fine thankyou $PERSON1"
+
