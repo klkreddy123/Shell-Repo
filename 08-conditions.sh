786897edde7a6cb60cfddf2c6d2b7aed88bf3349
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2024-03-08 10:57:35 +0530

    08-condition updated

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-Number=$Number
+Number=$1
 
 if [$Number -gt 10 ]
 
 echo "Entered number $Number is greater than 10"
-
+exit 1
 else
-
 echo "Entered number $Number is lesser than 10"
-
 fi
